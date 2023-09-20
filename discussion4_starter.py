a02dd64f4ce3b07fcf974a05f4c545f24b291fd0
Vivek Chanduri <vivek@Viveks-MacBook-Air.local>
2023-09-20 18:49:05 -0400

    First

--- discussion4_starter.py
+++ discussion4_starter.py
@@ -6,7 +6,10 @@ class Rectangle():
     # It sets an instance variable, "height" to the passed argument, height
 
     # YOUR CODE HERE
-
+    def _init_(self, width, height):
+        self.width = width
+        self.height = height
+        
 
 
     # Create the "__str__" method
@@ -15,7 +18,10 @@ class Rectangle():
     #       "A rectangle with width ____ and height ____"
 
     # YOUR CODE HERE
-
+    def _str_(self):
+        return "A rectangle with width" + str(self.width) + "and height" + str(self.height)
+    
+    
 
 
     # Create the "verify_input" method
